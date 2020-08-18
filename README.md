5f53dd9d902c6eccf28bd15c382732bd5dbbc116
trungtruc301297 <truc.tran@fiisoft.com>
2020-08-18 20:03:41 +0700

    truc

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Basic Flask App run in heroku
-$ heroku login
+# Basic Flask App
 
-$ heroku git:clone -a truc301297
-
-$ cd truc301297
-
-$ git add .
-
-$ git commit -am "make it better"
-
-$ git push heroku master
-
-
-# 1. Procfile: (use run in heroku: careful)
-web: gunicorn app:app
+`$ python app.py`
--- app.py
+++ app.py
@@ -1,63 +1,109 @@
   
 #Python libraries that we need to import for our bot
 import random
-from flask import Flask, request
+from flask import Flask, request, Response
 from pymessenger.bot import Bot
-
+from wit import Wit
+import requests
 app = Flask(__name__)
 # ACCESS_TOKEN = 'ACCESS_TOKEN'
 ACCESS_TOKEN = 'EAAIbv66Rh8wBAB7Kuffsbq1AQCH63y4Tm1MyTFQE5ZB5394LII1kFPGxS0heZASXtkmWR56oJDtcT6pS5iheEf7ZAY5i2HwqWUQVdfe0HamWz0HILtlGFouSLWEixUlHfzJayG1tTj1Uk5dUgbnRQW10pkzxfuQroTmpn86Ut5A7i29Czva'
 VERIFY_TOKEN = 'VERIFY_TOKEN'
+WIT_TOKEN    = '6OGZVMSELXTJM5HVSWH2IRBRRMLNGKGK'
 bot = Bot(ACCESS_TOKEN)
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
-# def receive_message():
-#     if request.method == 'GET':
-#         """Before allowing people to message your bot, Facebook has implemented a verify token
-#         that confirms all requests that your bot receives came from Facebook.""" 
-#         token_sent = request.args.get("hub.verify_token")
-#         return verify_fb_token(token_sent)
-#     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
-#     else:
-#         # get whatever message a user sent the bot
-#        output = request.get_json()
-#        for event in output['entry']:
-#           messaging = event['messaging']
-#           for message in messaging:
-#             if message.get('message'):
-#                 #Facebook Messenger ID for user so we know where to send response back to
-#                 recipient_id = message['sender']['id']
-#                 if message['message'].get('text'):
-#                     response_sent_text = get_message()
-#                     send_message(recipient_id, response_sent_text)
-#                 #if user sends us a GIF, photo,video, or any other non-text item
-#                 if message['message'].get('attachments'):
-#                     response_sent_nontext = get_message()
-#                     send_message(recipient_id, response_sent_nontext)
-#     return "Message Processed"
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook.""" 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        """
+        Handler for webhook (currently for postback and messages)
+        """
+        data = request.json
+        if data['object'] == 'page':
+            for entry in data['entry']:
+                # get all the messages
+                messages = entry['messaging']
+                if messages[0]:
+                    # Get the first message
+                    message = messages[0]
+                    # Yay! We got a new message!
+                    # We retrieve the Facebook user ID of the sender
+            
+                    fb_id = message['sender']['id']
+                    print('fb_id :', fb_id)
+                    # We retrieve the message content
+                    text = message['message']['text']
+                    print('text:', text)
+                    # Let's forward the message to Wit /message
+                    # and customize our response to the message in handle_message
+                    response = client.message(msg=text, context={'session_id':fb_id})
+                    # response ={'text': 'hello bot', 'intents': [{'id': '742387059668438', 'name': 'greating', 'confidence': 0.9787}], 'entities': {}, 'traits': {'wit$greetings': [{'id': '5900cc2d-41b7-45b2-b21f-b950d3ae3c5c', 'value': 'true', 'confidence': 0.9997}]}}
+                    handle_message(response=response, fb_id=fb_id)
+        else:
+
+            return 'Received Different Event'
+    return 'Message Processed!!!'
 
 
-# def verify_fb_token(token_sent):
-#     #take token sent by facebook and verify it matches the verify token you sent
-#     #if they match, allow the request, else return an error 
-#     if token_sent == VERIFY_TOKEN:
-#         return request.args.get("hub.challenge")
-#     return 'Invalid verification token'
+def verify_fb_token(token_sent):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error 
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+def fb_message(sender_id, text):
+    """
+    Function for returning response to messenger
+    """
+    data = {
+        'recipient': {'id': sender_id},
+        'message': {'text': text}
+    }
+    # Setup the query string with your PAGE TOKEN
+    qs = 'access_token=' + ACCESS_TOKEN
+    # Send POST request to messenger
+    resp = requests.post('https://graph.facebook.com/me/messages?' + qs,
+                         json=data)
+    return resp.content
+
+def first_trait_value(traits, trait):
+    """
+    Returns first trait value
+    """
+    if trait not in traits:
+        return None
+    val = traits[trait][0]['value']
+    if not val:
+        return None
+    return val
 
 
-# #chooses a random message to send to the user
-# def get_message():
-#     sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-#     # return selected item to the user
-#     return random.choice(sample_responses)
+def handle_message(response, fb_id):
+    """
+    Customizes our response to the message and sends it
+    """
+    # Checks if user's message is a greeting
+    # Otherwise we will just repeat what they sent us
+    greetings = first_trait_value(response['traits'], 'wit$greetings')
+    if greetings:
+        text = "hello!"
+    else:
+        text = "We've received your message: " + response['_text']
+    # send message
+    fb_message(fb_id, text)
 
-# #uses PyMessenger to send response to user
-# def send_message(recipient_id, response):
-#     #sends user the text message provided via input response parameter
-#     bot.send_text_message(recipient_id, response)
-#     return "success"
-def show():
-    return ' Hello truc!!'
+# Setup Wit Client
+client = Wit(access_token=WIT_TOKEN)
+resp = client.message('hello bot truc ngu ')
+print('Yay, got Wit.ai response: ' + str(resp))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug= True, host="0.0.0.0", port=8080)
